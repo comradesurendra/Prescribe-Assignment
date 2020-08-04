e4419b8743080ee526a835680ca79cc11beaf99e
Surendra Kumar <32401892+comradesurendra@users.noreply.github.com>
2020-08-04 17:50:35 +0530

    added feature

--- src/pages/Click.js
+++ src/pages/Click.js
@@ -5,12 +5,12 @@ import './click.css'
 export default class Click extends Component {
   render() {
     return (
-      <>
+      <div className='click-container'>
         <Header />
         <br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <button className="btn btn-danger mr-2" type="button">First Button</button>
         <button className="btn btn-danger mr-2" type="button">Second Button</button>
-      </>
+      </div>
     );
   }
 }
