5998235487ad3141286aa20349ee9283aa20f9d4
Surendra Kumar <32401892+comradesurendra@users.noreply.github.com>
2020-08-04 14:44:20 +0530

    UI Fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="pt-5">
       <div className="container text-center">
-        <p>&copy; Chatty 2020.</p>
+        <p>&copy; Click First 2020.</p>
       </div>
     </footer>
   )
